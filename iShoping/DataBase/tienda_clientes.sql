398df0717355c43bfda06da6d47b7f9a960d5501
danielcadavi <47570742+danielcadavi@users.noreply.github.com>
2021-02-07 13:15:02 -0500

    Pagina principal

---
+++ iShoping/DataBase/tienda_clientes.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (id, username, password, name) VALUES (1, 'herreraa', '123456', 'felipe');
+INSERT INTO clientes (id, username, password, name) VALUES (6, 'test', 'test', 'test');
+INSERT INTO clientes (id, username, password, name) VALUES (8, 'Aleja', '123456', 'Aleja Coral');
+INSERT INTO clientes (id, username, password, name) VALUES (9, 'sebas', '1234', 'sebas');
+INSERT INTO clientes (id, username, password, name) VALUES (10, 'daniel', '12345', 'daniel cadavid');
